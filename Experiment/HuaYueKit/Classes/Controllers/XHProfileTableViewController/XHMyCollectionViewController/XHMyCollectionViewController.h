e9ca0c2b97502aa01127376d1fac334e89804eb0
Mingkui.liu <754925822@qq.com>
2016-06-21 21:27:32 +0800

    update

---
+++ Experiment/HuaYueKit/Classes/Controllers/XHProfileTableViewController/XHMyCollectionViewController/XHMyCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  XHMyCollectionViewController.h
+//  HuaYue
+//
+//  Created by Gideon on 16/6/21.
+//  Copyright © 2016年 麦子收割队. All rights reserved.
+//
+
+#import "XHBaseTableViewController.h"
+
+@interface XHMyCollectionViewController : XHBaseTableViewController
+
+@end
